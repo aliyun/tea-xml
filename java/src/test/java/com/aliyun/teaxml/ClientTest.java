33fbce5c6bc0afc2ab2bd824acb985c77c931cec
sonny <wb-szq493230@alibaba-inc.com>
2020-03-02 16:06:25 +0800

    add java readme

--- java/src/test/java/com/aliyun/teaxml/ClientTest.java
+++ java/src/test/java/com/aliyun/teaxml/ClientTest.java
@@ -33,25 +33,25 @@ public class ClientTest {
                 "  <Contents>\n" +
                 "    <Key>run.sh</Key>\n" +
                 "    <LastModified>2019-11-18T07:18:33.000Z</LastModified>\n" +
-                "    <ETag>\"7FEEB4D16A1095FC5FAFFC6B5DA5A5B7\"</ETag>\n" +
+                "    <ETag>\"test\"</ETag>\n" +
                 "    <Type>Normal</Type>\n" +
                 "    <Size>4527</Size>\n" +
                 "    <StorageClass>Standard</StorageClass>\n" +
                 "    <Owner>\n" +
-                "      <ID>1325847523475998</ID>\n" +
-                "      <DisplayName>1325847523475998</DisplayName>\n" +
+                "      <ID>test</ID>\n" +
+                "      <DisplayName>test</DisplayName>\n" +
                 "    </Owner>\n" +
                 "  </Contents>\n" +
                 "  <Contents>\n" +
                 "    <Key>task.sh</Key>\n" +
                 "    <LastModified>2019-11-18T07:18:27.000Z</LastModified>\n" +
-                "    <ETag>\"094ACA03805A13CB8FE559A6DF615E3A\"</ETag>\n" +
+                "    <ETag>\"test\"</ETag>\n" +
                 "    <Type>Normal</Type>\n" +
                 "    <Size>603</Size>\n" +
                 "    <StorageClass>Standard</StorageClass>\n" +
                 "    <Owner>\n" +
-                "      <ID>1325847523475998</ID>\n" +
-                "      <DisplayName>1325847523475998</DisplayName>\n" +
+                "      <ID>test</ID>\n" +
+                "      <DisplayName>test</DisplayName>\n" +
                 "    </Owner>\n" +
                 "  </Contents>\n" +
                 "</ListBucketResult>";
