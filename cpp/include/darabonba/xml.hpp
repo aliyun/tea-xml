ac266da810d8b840b0099e3e5da6a290e43f8e67
wb-hx510875 <wb-hx510875@alibaba-inc.com>
2021-04-06 17:18:43 +0800

    fix: param type

--- cpp/include/darabonba/xml.hpp
+++ cpp/include/darabonba/xml.hpp
@@ -12,8 +12,8 @@ using namespace std;
 namespace Darabonba_XML {
 class Client {
 public:
-  static map<string, boost::any> parseXml(const shared_ptr<string>& body, const shared_ptr<void>& response);
-  static string toXML(shared_ptr<map<string, boost::any>> body);
+  static map<string, boost::any> parseXml(std::shared_ptr<string> body, std::shared_ptr<void> response);
+  static string toXML(std::shared_ptr<map<string, boost::any>> body);
 
   Client() {};
   ~Client() {};
